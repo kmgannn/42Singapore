483e80c20d2adcb00082187837852bb416d8003a
kmgannn <gankming@gmail.com>
2026-05-29 16:01:17 +0800

    c00 exercise

---
+++ c00/ft_print_numbers.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_is_negative(int n)
+{
+    if (n  >= 0)
+    {
+        ft_putchar('+');
+    }
+    else if (n  < 0)
+    {
+        ft_putchar('-');
+    }
+}
+
+
+int main(void)
+{
+    ft_is_negative(0);
+    return 0;
+}
